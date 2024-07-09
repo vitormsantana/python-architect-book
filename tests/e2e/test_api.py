0c26e6d12c8a500b5c88d5f180f489125177a638
Vitor Santana <vitormsantana25@gmail.com>
2024-07-09 11:39:44 -0300

    fix: adjusting some tests and classes batch and repo. adding tests from the book

--- tests/e2e/test_api.py
+++ tests/e2e/test_api.py
@@ -81,16 +81,16 @@ def test_400_message_for_invalid_sku(add_stock):
     assert r.status_code == 400
     assert r.json()['message'] == f'Invalid sku {unknown_sku}'
 
-def test_happy_path_returns_201_and_allocated_batch(add_stock):
+@pytest.mark.usefixtures('postgres_db')
+@pytest.mark.usefixtures('restart_api')
+def test_happy_path_returns_201_and_allocated_batch():
     sku, othersku = random_sku(), random_sku('other')
     earlybatch = random_batchref(1)
     laterbatch = random_batchref(2)
     otherbatch = random_batchref(3)
-    add_stock([
-        (laterbatch, sku, 100, '2011-01-02'),
-        (earlybatch, sku, 100, '2011-01-01'),
-        (otherbatch, othersku, 100, None),
-        ])
+    post_to_add_batch(laterbatch, sku, 100, '2011-01-02')
+    post_to_add_batch(earlybatch, sku, 100, '2011-01-01')
+    post_to_add_batch(otherbatch, othersku, 100, None)
     data = {'orderid': random_orderid(), 'sku': sku, 'qty': 3}
     url = config.get_api_url()
     r = requests.post(f'{url}/allocate', json=data)
@@ -105,3 +105,15 @@ def test_unhappy_path_returns_400_and_error_message():
     r = requests.post(f'{url}/allocate', json=data)
     assert r.status_code == 400
     assert r.json()['message'] == f'Invalid sku {unknown_sku}'
+
+def post_to_add_batch(ref, sku, qty, eta):
+    url = config.get_api_url()
+    r = requests.post(
+        f'{url}/add_batch',
+        json={'ref': ref, 'sku': sku, 'qty': qty, 'eta': eta}
+    )
+    assert r.status_code == 201
+    
+
+
+
--- tests/unit/test_services.py
+++ tests/unit/test_services.py
@@ -1,21 +1,27 @@
 from adapters import repository
 import pytest
-from domain import model
 from service_layer import services
-from domain.model import Batch
+from domain.model import OrderLine, Batch, allocate
+from datetime import date, timedelta
 
 class FakeRepository(repository.AbstractRepository):
     def __init__(self, batches):
         self._batches = set(batches)
 
     def add(self, batch):
-        self._batches.add(Batch)
+        self._batches.add(batch)
 
     def get(self, reference):
         return next(b for b in self._batches if b.reference == reference)
-    
+
     def list(self):
         return list(self._batches)
+
+    @staticmethod
+    def for_batch(ref, sku, qty, eta=None):
+        return FakeRepository([
+            Batch(ref, sku, qty, eta)
+        ])
     
 class FakeSession():
     commited = False
@@ -23,27 +29,64 @@ class FakeSession():
     def commit(self):
         self.commited = True
 
-def test_returns_allocation():
-    line = model.OrderLine("o1", "COMPLICATED-LAMP", 10)
-    batch = model.Batch("b1", "COMPLICATED-LAMP", 100, eta=None)
-    repo = FakeRepository([batch])
+tomorrow = date(2021, 1, 2)    # Arbitrary date
 
-    result = services.allocate(line, repo, FakeSession())
-    assert result == "b1"
+def test_returns_allocation():
+    repo = FakeRepository.for_batch("batch1", "COMPLICATED-LAMP", 100, eta=None)
+    result = services.allocate("o1", "COMPLICATED-LAMP", 10, repo, FakeSession())
+    assert result == "batch1"
 
 def test_error_for_invalid_sku():
-    line = model.OrderLine("o1", "NNNNNN", 10)
-    batch = model.Batch("b1", "SSSSS", 100, eta=None)
+    line = OrderLine("o1", "NONEXISTENTSKU", 10)
+    batch = Batch("b1", "SSSSS", 100, eta=None)
     repo = FakeRepository([batch])
 
-    with pytest.raises(services.InvalidSku, match="Invalid sku NNNNNN"):
-        services.allocate(line, repo, FakeSession())
+    with pytest.raises(services.InvalidSku, match="Invalid sku NONEXISTENTSKU"):
+        services.allocate("o1", "NONEXISTENTSKU", 10, repo, FakeSession())
 
 def test_commits():
-    line = model.OrderLine('o1', 'OMINOUS-MIRROR', 10)
-    batch = model.Batch('b1', 'OMINOUS-MIRROR', 100, eta=None)
+    batch = Batch('b1', 'OMINOUS-MIRROR', 100, eta=None)
     repo = FakeRepository([batch])
     session = FakeSession()
 
-    services.allocate(line, repo, session)
+    services.allocate("o1", "OMINOUS-MIRROR", 10, repo, session)
     assert session.commited is True
+
+def test_prefers_current_stock_batches_to_shipments():
+    in_stock_batch = Batch("in-stock-batch", "RETRO-CLOCK", 100, eta=None)
+    shipment_batch = Batch("shipment-batch", "RETRO-CLOCK", 100, eta=tomorrow)
+    line = OrderLine("oref", "RETRO-CLOCK", 10)
+
+    allocate(line, [in_stock_batch, shipment_batch])
+
+    assert in_stock_batch.available_quantity == 90
+    assert shipment_batch.available_quantity == 100
+
+def test_prefers_warehouse_batches_to_shipments():
+    in_stock_batch = Batch("in-stock-batch", "RETRO-CLOCK", 100, eta=None)
+    shipment_batch = Batch("shipment-batch", "RETRO-CLOCK", 100, eta=tomorrow)
+    repo = FakeRepository([in_stock_batch, shipment_batch])
+    session = FakeSession()
+    services.allocate("oref", "RETRO-CLOCK", 10, repo, session)
+
+    assert in_stock_batch.available_quantity == 90
+    assert shipment_batch.available_quantity == 100
+
+def test_add_batch():
+    repo, session = FakeRepository([]), FakeSession()
+    services.add_batch("b1", "CRUNCHY-ARMCHAIR", 100, None, repo, session)
+    assert repo.get("b1") is not None
+    assert session.commited
+
+def test_allocate_returns_allocation():
+    repo, session = FakeRepository([]), FakeSession()
+    services.add_batch("batch1", "COMPLICATED-LAMP", 100, None, repo, session)
+    result = services.allocate("o1", "COMPLICATED-LAMP", 10, repo, session)
+    assert result == "batch1"
+
+def test_allocate_errors_for_invalid_sku():
+    repo, session = FakeRepository([]), FakeSession()
+    services.add_batch("batch1", "AREALSKU", 100, None, repo, session)
+
+    with pytest.raises(services.InvalidSku, match="Invalid sku NONEXISTENTSKU"):
+        services.allocate("o1", "NONEXISTENTSKU", 10, repo, FakeSession())
